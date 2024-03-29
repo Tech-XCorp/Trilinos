e9a1475dfd5198fa9b4ea3a5444c6b37a09c0926
Ben Cowan <ben@alturasresearch.com>
2024-03-29 13:00:19 -0600

    Make sure added flags are for the correct languages; find MPI library even when not on Windows.

--- cmake/tribits/core/std_tpls/FindTPLMPI.cmake
+++ cmake/tribits/core/std_tpls/FindTPLMPI.cmake
@@ -43,7 +43,7 @@ global_set(TPL_MPI_INCLUDE_DIRS "")
 global_set(TPL_MPI_LIBRARIES "")
 global_set(TPL_MPI_LIBRARY_DIRS "")
 
-if(WIN32 AND TPL_ENABLE_MPI)
+if(TPL_ENABLE_MPI)
   find_package(MPI)
   include_directories(${MPI_INCLUDE_PATH})
   global_set(TPL_MPI_INCLUDE_DIRS ${MPI_INCLUDE_PATH})
